41fa0f275c1d8e2dd02c9a5c0b376d00b0828a23
Andreas Axtelius <andaxt@gmail.com>
2021-01-04 16:54:36 +0100

    Basic playercontroller and cameracontroller
    Supports "wasd" movement and the camera controller is made to simply follow an object.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject trackedObject;
+
+    public float cameraHeight = 10;
+    public float cameraCenteringX = 0;
+    public float cameraCenteringZ = -2;
+
+    public float cameraTilt = 80;
+    public float cameraPan = 0;
+    public float cameraYaw = 0;
+
+    private Vector3 cameraOffset;
+    private Quaternion cameraRotation;
+
+    private Vector3 defaultOffset;
+    private Quaternion defaultRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Calculate and set default offset based on settings
+        defaultOffset = new Vector3(cameraCenteringX, cameraHeight, cameraCenteringZ);
+        transform.position = trackedObject.transform.position + defaultOffset;
+
+        // Calculate and set default camera rotation based on settings
+        defaultRotation = Quaternion.Euler(cameraTilt, cameraPan, cameraYaw);
+        transform.rotation = defaultRotation;
+    }
+
+    // LateUpdate is called once per frame, after Update().
+    void LateUpdate()
+    {
+        transform.position = trackedObject.transform.position + defaultOffset;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // FixedUpdate is called at a fixed interval independently of frame rate.
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    /// <summary>
+    /// To be executed in update. Moves the player according to current input.
+    /// </summary>
+    void MovePlayer()
+    {
+        // Read player input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Calculate new movement vector
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        // Move the player object
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+}
