dacedfd9f83b0339307e88156f72c0f29f7eb97f
traxor <traxor@users.noreply.github.com>
2021-01-16 21:39:46 +0100

    Added camera smoothing (#3)
    Co-authored-by: Traxor <unitraxor@gmail.com>

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
 {
     public GameObject trackedObject;
 
+    public float maxCameraHeightOffset = 5;
     public float cameraHeight = 10;
     public float cameraCenteringX = 0;
     public float cameraCenteringZ = -2;
@@ -19,6 +20,9 @@ public class CameraController : MonoBehaviour
 
     private Vector3 defaultOffset;
     private Quaternion defaultRotation;
+
+    public float smoothTime = 0.1F;
+    private Vector3 velocity = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -39,6 +43,10 @@ public class CameraController : MonoBehaviour
         float y = defaultOffset.y;
         float z = trackedObject.transform.position.z + defaultOffset.z;
 
-        transform.position = new Vector3(x, y, z);
+        // Only consider the speed in x and y axis
+        float speed = (new Vector3(transform.position.x, 0, transform.position.z) - new Vector3(x, 0, z)).magnitude;
+
+        Vector3 targetPosition = new Vector3(x, Mathf.Min(y + (speed), y + maxCameraHeightOffset), z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
