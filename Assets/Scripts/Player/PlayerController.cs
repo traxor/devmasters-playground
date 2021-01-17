bfeea2e80f529aa12b3177f0a33faa4ec10bb096
Andreas Axtelius <andaxt@gmail.com>
2021-01-17 17:20:02 +0100

    Adjusted and combined commit of branch f/UpdatePlayerModel

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,43 +4,109 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5;
-    public float maxSpeed = 10;
+    public Animator animator;
+    public CharacterController characterController;
+    public float rotationSpeedInRadians = 10;
+    public float runSpeed = 8f;
+    public float walkSpeed = 5f;
 
-    private Rigidbody body;
- 
-    // Start is called before the first frame update
+    //private Vector3 playerVelocity = Vector3.zero;
+    private Vector3 movementInput = Vector3.zero;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
     void Start()
     {
-        body = GetComponent<Rigidbody>();
-
-        if (body == null)
-        {
-            Debug.LogError("Failed to get Rigidbody component!");
-            return;
-        }
-    }
-
-    // Update is called for each frame
-    void Update()
-    {
-        MovePlayer();
+        characterController = gameObject.GetComponent<CharacterController>();
     }
 
     /// <summary>
-    /// To be executed in update. Moves the player according to current input.
+    /// Update is called for each frame.
+    /// </summary>
+    void Update()
+    {
+        MovePlayer();
+        RotatePlayer();
+        SetMovementAnimation();
+    }
+
+    /// <summary>
+    /// To be executed in update. Moves the character according to current input.
+    /// Handles gravity and jumping.
     /// </summary>
     void MovePlayer()
     {
-        // Read player input
+        // TODO: Code below is a preparation for jumping mechanics
+        // Remove all negative Y-axis velocity if already grounded
+        //if (characterController.isGrounded && playerVelocity.y < 0)
+        //{
+        //    playerVelocity.y = 0f;
+        //}
+
+        // Read input
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
 
-        // Calculate new movement vector
-        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        // Calculate player movement speed and move accordingly
+        Vector3 movementDirection = new Vector3(moveHorizontal, 0, moveVertical);
 
-        // Move the player object
-        Vector3 clampedSpeed = Vector3.ClampMagnitude(movement * moveSpeed, maxSpeed);
-        body.AddForce(clampedSpeed);
+        if (isRunning)
+        {
+            movementInput = movementDirection * runSpeed;
+        }
+        else
+        {
+            movementInput = movementDirection * walkSpeed;
+        }
+        characterController.Move(movementInput * Time.deltaTime);
+
+        // Calculate and add gravity
+        Vector3 gravitySpeed;
+
+        if (characterController.isGrounded)
+        {
+            gravitySpeed = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            gravitySpeed = new Vector3(0, Physics.gravity.y, 0);
+        }
+        characterController.Move(gravitySpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Tries to rotate the player towards the current movement direction.
+    /// Rotation is limited by the player's maximum rotation speed.
+    /// </summary>
+    void RotatePlayer()
+    {
+        if (Vector3.zero == movementInput)
+        {
+            // If we're not moving anywhere, don't rotate.
+            return;
+        }
+
+        Vector3 targetDirection = movementInput.normalized;
+        Debug.DrawRay(transform.position, targetDirection, Color.red);
+
+        float rotationStep = rotationSpeedInRadians * Time.deltaTime;
+
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, rotationStep, 0.0f);
+        Debug.DrawRay(transform.position, targetDirection, Color.green);
+
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+    /// <summary>
+    /// Gets the maximum magnitude of the movement directions to decide if
+    /// the character should be running, walking, or standing idle.
+    /// </summary>
+    void SetMovementAnimation()
+    {
+        float maxMovement = Mathf.Max(Mathf.Abs(movementInput.x),
+                                      Mathf.Abs(movementInput.z));
+        animator.SetFloat("MovementSpeed", maxMovement);
     }
 }
